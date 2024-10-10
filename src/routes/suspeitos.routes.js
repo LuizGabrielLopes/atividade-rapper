e9428562965bc34eb264b7618e30a0f9890a3a95
Luiz Gabriel <luiz.gabriellopesc@gmail.com>
2024-10-10 09:54:52 -0300

    feat: create post method

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -46,4 +46,30 @@ suspeitosRoutes.get("/", (req, res) => {
     return res.status(200).send({ suspeitos })
 });
 
+//Rota para criar um novo suspeito
+suspeitosRoutes.post("/", (req, res) => {
+    const { nome, idade, descricao, envolvimento } = req.body;
+    if (!nome || !Number.isInteger(idade)) {
+        return res.status(400).json({
+            message: "O nome é obrigatório e a idade deve ser um número inteiro"
+        })
+    }
+
+    if (envolvimento != "sim" && envolvimento != "não") {
+        return res.status(400).send({
+            message: "Digite sim ou não"
+        })
+    }
+
+    const novoSuspeito = {
+        id: Number(Math.floor(Math.random() * 100)),
+        nome,
+        idade,
+        descricao,
+        envolvimento,
+    }
+    suspeitos.push(novoSuspeito)
+    return res.status(201).send({ suspeitos })
+});
+
 export default suspeitosRoutes;
